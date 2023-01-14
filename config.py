b02e8b7fb4a4a3e29719c5c6a475b0eeaa1f15cb
GrigoriyPA <79596613+GrigoriyPA@users.noreply.github.com>
2023-01-14 14:49:06 +0500

    Add files via upload

---
+++ config.py
@@ -0,0 +1,14 @@
+import datetime
+
+
+TELEGRAM_TOKEN = ""
+ADMINS = {}
+
+USERS_INFORMATION_DB_NAME = "data/users_inf.db"
+GEOLOCATION_PATH = "data/map.jfif"
+
+TIMEZONE = datetime.timedelta(hours=0)
+TIME_STEP = datetime.timedelta(minutes=15)
+SERVICE_TIME = datetime.timedelta(hours=1)
+WORKING_HOURS = [(datetime.time(hour=11), datetime.time(hour=20))]
+DESC_WIGTH = 61
